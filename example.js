61862a70fc084f9f8fe8dd4bb543c7e87fe17a0d
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-08-21 13:34:24 +0200

    VSCode dev configuration, added mousemove event to example file

--- example.js
+++ example.js
@@ -4,3 +4,7 @@ screens2d.canvas.rect(0, 0, "full", 50, 0, "brown")
 screens2d.canvas.text(20, 30, "Roboto", "500 18px", "About kangaroos", "white")
 screens2d.canvas.text(20, 80, "Roboto", "16px", "I like kangaroos.", "black")
 screens2d.canvas.image(20, 100, "kangaroo.jpeg", 200, 300)
+
+document.addEventListener('mousemove', (e) => {
+    screens2d.canvas.rect(e.clientX-5, e.clientY-5, 10, 10, 5, "black")
+});
